f40e845060736b406290bd3f1aacfa77396c4704
SUMIT KUMAR <120464532+SumitKumar777@users.noreply.github.com>
2026-01-11 20:09:34 +0530

    feat:prisma_setUp

---
+++ packages/db/src/index.ts
@@ -0,0 +1,33 @@
+import { config } from "dotenv";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+config({ path: join(__dirname, "../../.env") });
+
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+
+export const prisma =
+  globalThis.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["query", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export default prisma;
